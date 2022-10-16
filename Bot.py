c19bc06a31e14e3f8a15c23984cc3629952826a7
morispolanco <morispolanco@gmail.com>
2022-10-16 13:49:13 -0600

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -10,18 +10,6 @@ restart_sequence = "\n\Humano:"
 
 session_prompt = "Lo que sigue es una conversación con un asistente de AI experto en vinos. El asistente es servicial, creativo, inteligente y muy amable.\n\nHumano: Hola, ¿quién eres?\nAI: Soy un bot de AI experto en enología. ¿En qué puedo ayudarle hoy? \nHumano: ¿Qué debo buscar  en un vino tinto?\nAI: Al buscar un vino tinto, querrá asegurarse de encontrar una buena variedad que se adapte a sus gustos. También es importante considerar el precio y el tipo de vino que está buscando.\nHumano: ¿Qué es el proseco?\nAI: El proseco es un tipo de vino espumoso que se produce en Italia. Tiene un sabor dulce y afrutado, y suele ser más ligero que otros tipos de vino espumoso.\n"
 
-import streamlit as st
-
-st.title('Counter Example')
-if 'count' not in st.session_state:
-    st.session_state.count = 0
-
-increment = st.button('Increment')
-if increment:
-    st.session_state.count += 1
-
-st.write('Count = ', st.session_state.count)
-
 def mises(question, chat_log=None):
     prompt_text = f'{chat_log}{restart_sequence}: {question}{start_sequence}:'
     response = openai.Completion.create(
